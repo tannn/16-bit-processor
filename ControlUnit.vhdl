a9f41be5e713238264d579a4c08682d754b4c91a
Cwalrus96 <Caleb.compton@gmail.com>
2016-04-04 19:49:17 -0500

    Edited Control Bits

--- ControlUnit.vhdl
+++ ControlUnit.vhdl
@@ -19,7 +19,7 @@ ENTITY ControlUnit IS
 	SIGNAL wmfc: STD_LOGIC; 
 	shared VARIABLE stage: integer := 0;
 	BEGIN 
-		PROCESS(clock, reset)
+		PROCESS(clock, reset, opCode)
 		BEGIN 
 			IF (rising_edge(clock)) THEN 
 				IF (reset = '1') THEN 
@@ -85,18 +85,68 @@ ENTITY ControlUnit IS
 						alu_op <= "11"; 
 						b_select <= '1';
 						extend <= "10"; 
+						
+					ELSIF(opCode(4 downto 0)= "00110" )THEN 
+						--LD
+						alu_op <= "11"; 
+						b_select <= '1';
+						extend <= "01"; 
+						
+					ELSIF(opCode(4 downto 0) = "00011") THEN 
+						--ST
+						alu_op <= "11" ;
+						extend <= "01" ;
+						B_select <= '1' ;
+					ELSIF(opCode(4 downto 0) = "01000") THEN 
+						--DEC 
+						extend <= "11";
+						alu_op <= "11"; 
+						b_select <= '1'; 
+					ELSIF(opCode(4 downto 0) = "01011" )THEN
+						--CLR
+						alu_op <= "00"; 
+					ELSIF(opCode(4 downto 0) = "01010") THEN 
+						--JR 
+						pc_enable <= '1';	
+						pc_select <= '0';  
+					ELSIF(opCode(4 downto 0) = "10000") THEN
+						--J
+						pc_enable <= '1'; 
+						inc_select <= '1'; 
+					ELSIF(opCode(4 downto 0) = "10001") THEN
+						--J.L 
+						pc_enable <= '1'; 
+						inc_select <= '1';
+						
 					END IF; 
 				
 				--Stage 4  (Memory Stage)
 				
 				ELSIF(stage = 4)THEN
-					--Double Operand 
-					IF(opCode (4 downto 0) = "11111")THEN 
-						--DO NOTHING YET
- 					
+				
+					--Double Operand
+					IF(opCode (4 downto 0) = "00011" )THEN
+						--ST
+						mem_write <= '1' ;  
+						ma_select <= '0'; 
+					ELSIF(opCode (4 downto 0) = "00110" )THEN
+						--LD
+						mem_read <= '1'; 
+						ma_select <= '0'; 
+						y_select <= "01"; 
+						
 					--Single Operand 
-					ELSIF(opCode (4 downto 0)= "11111")THEN 
-						--DO NOTHING YET 
+					ELSIF(opCode(4 downto 0) = "01010") THEN 
+						--JR 
+						pc_enable <= '0';	
+					ELSIF(opCode(4 downto 0) = "10000") THEN
+						--J
+						pc_enable <= '0'; 
+					ELSIF(opCode(4 downto 0) = "10001") THEN
+						--J.L 
+						pc_enable <= '0'; 
+						y_select <= "10"; 
+						
 						
 					END IF; 
 				
@@ -106,17 +156,32 @@ ENTITY ControlUnit IS
 					--Double Operand 
 					IF(opCode (4) = '0' AND opCode (3) = '0'   )THEN 
 						rf_write <= '1'; 
- 					
+ 						--ST
+						mem_write <= '0';
+					ELSIF(opCode(4 downto 0) = "00110") THEN 
+						--LD
+						mem_write <= '0'; 
+					ELSIF(opCode(4 downto 0) = "01000" ) THEN 
+					
 					--Single Operand 
 					ELSIF(opCode (4) = '0' AND opCode (3) = '1' )THEN 
 						rf_write <= '1'; 
+					ELSIF(opCode(4 downto 0) = "00111") THEN 
+						--DEC 
+						rf_write <= '1'; 
+					ELSIF(opCode(4 downto 0) = "01011" )THEN 
+						--CLR 
+						rf_write <= '1'; 
+					ELSIF(opCode(4 downto 0) = "10001") THEN
+						--J.L 
+						rf_write <= '1';  
 						
 					END IF; 
 				 
 				
-			END IF; 
+				END IF; 
 		
-		END IF; 
+			END IF;
 			
 		END PROCESS; 
 		
